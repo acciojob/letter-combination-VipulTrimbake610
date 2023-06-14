1b99ce96847edc9b33a5d5752379ae208d28428f
Vipul Trimbake <130178066+VipulTrimbake610@users.noreply.github.com>
2023-06-14 10:48:27 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function letterCombinations(input_digit) {
   //Complete the function
+	let arr = {
+		0:"0",
+		1:'1',
+		2:'abc',
+		3:'def',
+		4:'ghi',
+		5:'jkl',
+		6:'mno',
+		7:'pqrs',
+		8:'tuv',
+		9:'wxyz']
 }
 
 module.exports = letterCombinations;
